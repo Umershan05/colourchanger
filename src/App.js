bbfbb59b340ffd2900db61024494fcc8e6b2b16c
umershan05 <umershan07@gmail.com>
2023-10-26 14:01:18 -1200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+
+import { Button } from 'react-bootstrap';
 import './App.css';
 
 function App() {
+  const [bgColor, setBgColor] = useState('#FFFFFF'); 
+
+  const changeBackgroundColor = () => {
+   
+    const randomColor = "#" + ((1 << 24) * Math.random() | 0).toString(16);
+    setBgColor(randomColor);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{height:"100vh", backgroundColor: bgColor }} className='d-flex justify-content-center align-items-center '>
+    <div className='d-flex justify-content-center align-items-center '>  
+    <Button onClick={changeBackgroundColor} type="submit" className='shadow btn text-light w-100 rounded bg-danger' variant="">Change Color</Button>
+      </div>
     </div>
   );
 }
